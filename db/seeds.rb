9388495a6912fa28d656823b66eb23ea8754d890
Vitor Sales <vitor.salles.santos05@gmail.com>
2022-07-28 14:37:09 -0300

    Restauração de arquivos

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,34 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cadastrando moedas..."
+
+coins = [
+            {
+                description: "Bitcoin",
+                acronym: "BTC",
+                url_image: "https://toppng.com/public/uploads/thumbnail/bitcoin-png-bitcoin-logo-transparent-background-115629339976fammkuch0.png"
+            },
+            {
+                description: "Ethereum",
+                
+                acronym: "ETH",
+                url_image: "https://s2.coinmarketcap.com/static/img/coins/200x200/1973.png"
+            },
+            {
+                description: "Dash",
+                acronym: "DASH",
+                url_image: "https://seeklogo.com/images/D/dash-logo-4A14989CF5-seeklogo.com.png"
+            },
+            {
+                description: "Tether",
+                acronym: "USTD",
+                url_image: "https://cdn-icons-png.flaticon.com/512/825/825508.png"
+            },
+            {
+                description: "Ripple",
+                acronym: "XRP",
+                url_image: "https://seeklogo.com/images/R/ripple-xrp-logo-E97D62205B-seeklogo.com.png"
+            }
+        ]
 
 
-Coin.create!(
-    description: "Bitcoin",
-    acronym: "BTC",
-    url_image: "https://toppng.com/public/uploads/thumbnail/bitcoin-png-bitcoin-logo-transparent-background-115629339976fammkuch0.png"
-)
+coins.each do |coin|
+    Coin.find_or_create_by!(coin)
+end
 
-Coin.create!(
-    description: "Ethereum",
-    acronym: "ETH",
-    url_image: "https://s2.coinmarketcap.com/static/img/coins/200x200/1973.png"
-)
 
-Coin.create!(
-    description: "Dash",
-    acronym: "DASH",
-    url_image: "https://seeklogo.com/images/D/dash-logo-4A14989CF5-seeklogo.com.png"
-)
-
-Coin.create!(
-    description: "Tether",
-    acronym: "USTD",
-    url_image: "https://cdn-icons-png.flaticon.com/512/825/825508.png"
-)
-
-Coin.create!(
-    description: "Ripple",
-    acronym: "XRP",
-    url_image: "https://seeklogo.com/images/R/ripple-xrp-logo-E97D62205B-seeklogo.com.png"
-)
+puts "Cadastradas com sucesso!"
--- lib/tasks/dev.rake
+++ lib/tasks/dev.rake
@@ -2,28 +2,23 @@ namespace :dev do
   desc "Configura o ambiente de desenvolvimento"
   task setup: :environment do
     if Rails.env.development?
-      spinner = TTY::Spinner.new("[:spinner] Apagando DB...")
-      spinner.auto_spin
-      %x(rails db:drop:_unsafe)
-      spinner.success("(Concluído com sucesso!)")
-      
-      spinner = TTY::Spinner.new("[:spinner] Criando DB...")
-      spinner.auto_spin
-      %x(rails db:create)
-      spinner.success("(Concluído com sucesso!)")
-      
-      spinner = TTY::Spinner.new("[:spinner] Migrando DB...")
-      spinner.auto_spin
-      %x(rails db:migrate)
-      spinner.success("(Concluído com sucesso!)")
-      
-      spinner = TTY::Spinner.new("[:spinner] Populando DB...")
-      spinner.auto_spin
-      %x(rails db:seed)
-      spinner.success("(Concluído com sucesso!)")
+      show_spinner("Apagando DB...") { %x(rails db:drop:_unsafe) }
+      show_spinner("Criando DB...") { %x(rails db:create) }
+      show_spinner("Migrando DB...") { %x(rails db:migrate) }
+      show_spinner("Populando DB...") { %x(rails db:seed) }
     else
       puts "Você deve estar no ambiente de desenvolvimento!"
     end
   end
-
+  
+  # 'private' faz com que todos os metodos presentes após ele possas ser utilizados
+  # apenas dentro da mesma classe classe
+  private
+  
+  def show_spinner(msg_start, msg_end = "Concluído!")
+    spinner = TTY::Spinner.new("[:spinner] #{msg_start}")
+    spinner.auto_spin
+    yield
+    spinner.success("(#{msg_end})")
+  end
 end
