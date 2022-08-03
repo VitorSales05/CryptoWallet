19a2d3ac2fb044265efd0e8627bbcabd2d3b0f00
Vitor Sales <vitor.salles.santos05@gmail.com>
2022-08-02 22:56:24 -0300

    Alterar idoma da página

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::Base
 
+# Manipula o idioma do site pelos parâmentro passados através da URL
+  before_action :set_locale
+  
+  def set_locale 
+    if params[:locale]
+      cookies[:locale] = params[:locale]
+    end
+    
+    if cookies[:locale]
+      if I18n.locale != cookies[:locale]
+        I18n.locale = cookies[:locale]
+      end
+    end
+  end
+
 end
